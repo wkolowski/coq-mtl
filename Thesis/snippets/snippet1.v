a33e1290c5cb38860c7c5a6aee83b23a6120ffe5
wkolowski <wkolowski@protonmail.com>
2019-06-11 19:34:37 +0200

    Thesis: got minted to work. Wrote an example Coq file for illustratory purposes. Started describing it.

---
+++ Thesis/snippets/snippet1.v
@@ -0,0 +1,52 @@
+Print nat.
+(*
+Inductive nat : Set :=
+    | O : nat
+    | S : nat -> nat.
+*)
+
+Inductive Tree (A : Type) : Type :=
+    | Leaf : A -> Tree A
+    | Node : Tree A -> Tree A -> Tree A.
+
+Arguments Leaf {A} _.
+Arguments Node {A} _ _.
+
+Fixpoint label
+  {A : Type} (t : Tree A) (n : nat) : nat * Tree (A * nat) :=
+match t with
+    | Leaf x => (n, Leaf (x, n))
+    | Node l r =>
+        let (n', l') := label l n in
+        let (n'', r') := label r (S n') in
+          (n'', Node  l' r')
+end.
+
+Definition lbl {A : Type} (t : Tree A) : Tree (A * nat) :=
+  snd (label t 0).
+
+Compute lbl (Node (Node (Leaf true) (Leaf true)) (Leaf false)).
+(* = Node (Node (Leaf (true, 0)) (Leaf (true, 1))) (Leaf (false, 2))
+   : Tree (bool * nat) *)
+
+Fixpoint size {A : Type} (t : Tree A) : nat :=
+match t with
+    | Leaf _ => 1
+    | Node l r => size l + size r
+end.
+
+Require Import Arith.
+
+Lemma label_size :
+  forall (A : Type) (t : Tree A) (n n' : nat) (t' : Tree (A * nat)),
+    label t n = (n', t') -> S n' = n + size t.
+Proof.
+  induction t as [| l IHl r IHr].
+    cbn. intros. inversion H. rewrite <- plus_comm. cbn. reflexivity.
+    cbn. intros.
+      case_eq (label l n); intros m1 t1 H1.
+      case_eq (label r (S m1)); intros m2 t2 H2.
+      specialize (IHl _ _ _ H1). specialize (IHr _ _ _ H2).
+      rewrite H1, H2 in H. inversion H; subst.
+      rewrite IHr, IHl. rewrite plus_assoc. reflexivity.
+Qed.
