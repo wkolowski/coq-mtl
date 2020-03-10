a1dedd749fb30667b65e11158f95839808a17ccb
wkolowski <wkolowski@protonmail.com>
2020-03-10 23:01:33 +0100

    Updated link to thesis' github repo.

--- Thesis/buildthesis.sh
+++ Thesis/buildthesis.sh
@@ -5,8 +5,8 @@
 dot Hierarchy.dot -Tpdf -o Hierarchy.pdf
 
 # Compile the thesis.
-latexmk -pdf -f -quiet -interaction=nonstopmode Thesis.tex
+latexmk -pdf -f -quiet -interaction=nonstopmode -shell-escape Thesis.tex
 
 # Compile the slides for thesis defense.
 cd Defense
-latexmk -pdf -f -quiet -interaction=nonstopmode Slides.tex
+latexmk -pdf -f -quiet -interaction=nonstopmode -shell-escape Slides.tex
