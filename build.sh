6006e61aec2590d66698b4b32f844bc6666a86b2
wkolowski <wkolowski@protonmail.com>
2022-04-09 17:37:51 +0200

    Formalizations of two papers moved from another project, because they depend on this library.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Generate a new makefile for all .v files in the library.
-coq_makefile -R "." CoqMTL -o makefile Base.v Control.v $(find Control Misc Parser Theory -name "*v")
+coq_makefile -R "." CoqMTL -o makefile Base.v Control.v $(find Control Misc Papers Parser Theory -name "*v")
 
 # Build the library.
 make
